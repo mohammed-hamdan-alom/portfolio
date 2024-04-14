00761fc401cfa4a08079a8369c5e81490103d1cf
mohammed-hamdan-alom <mohammed.hamdan.alom@gmail.com>
2024-04-14 22:41:54 +0100

    Initial commit

---
+++ src/components/main/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+      <video
+        autoPlay
+        muted
+        loop
+        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+      >
+        <source src="/blackhole.webm" type="video/webm" />
+      </video>
+    );
+};
+
+export default Hero;
---
+++ src/components/main/navbar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+const Navbar = () => {
+  const { setTheme } = useTheme();
+
+  return (
+    <div className="flex flex-row items-center w-full">
+      <div className="flex w-1/2 justify-start">
+        Mohammed Alom
+      </div>
+      <div className="flex w-1/2 justify-end">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
